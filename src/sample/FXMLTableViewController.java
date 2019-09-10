f655a81f72437dd76963cdba903fca06e617dc8f
SLamont <stlamont3134@eagle.fgcu..edu>
2019-09-09 21:43:49 -0400

    Initial commit

---
+++ src/sample/FXMLTableViewController.java
@@ -0,0 +1,34 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class FXMLTableViewController {
+    @FXML private TableView<Person> tableView;
+    @FXML
+    private TextField firstNameField;
+    @FXML private TextField lastNameField;
+    @FXML private TextField emailField;
+
+    @FXML
+    protected void addPerson(ActionEvent event) {
+        ObservableList<Person> data = tableView.getItems();
+        data.add(new Person(firstNameField.getText(),
+                lastNameField.getText(),
+                emailField.getText()
+        ));
+
+        firstNameField.setText("");
+        lastNameField.setText("");
+        emailField.setText("");
+    }
+}
+
